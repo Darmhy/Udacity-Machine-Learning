eb74a801aabafab04e7db93f43b0480e2cfcc36a
Darmhy <40070230+Darmhy@users.noreply.github.com>
2021-11-04 19:38:08 +0000

    Update Author_ID_NB.py

--- Author_ID_NB.py
+++ Author_ID_NB.py
@@ -20,6 +20,9 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+#########################################################
+### your code goes here ###
+
 """ compute the accuracy of your Naive Bayes classifier """
 ### import the sklearn module for GaussianNB
 from sklearn.naive_bayes import GaussianNB
